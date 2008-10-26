e59506db18fc1867f8516bac0c704d1421b790db
Scordo_cp <Scordo@users.noreply.github.com>
2008-10-26 17:21:59 +0000

    Added a logic to notify accounts that have been removed from the list of observers, so that those observers can detect an unwanted removal from the list of observer.

--- TFSWIWatcher.BL/Providers/TFSObserverAccountProvider.cs
+++ TFSWIWatcher.BL/Providers/TFSObserverAccountProvider.cs
@@ -56,45 +56,71 @@ namespace TFSWIWatcher.BL.Providers
 
 
             _log.DebugFormat("Getting field containing observers from workitem. Fieldname is: {0}", _config.ObserversFieldName);
-            Field observerField = workItem.Revisions[context.WorkItemRevision - 1].Fields[_config.ObserversFieldName];
+            Field currentObserverField = workItem.Revisions[context.WorkItemRevision - 1].Fields[_config.ObserversFieldName];
 
-            List<string> observers = new List<string>();
+            Field previousObserverField = null;
 
-            if (observerField != null)
+            if (workItem.Revisions.Count > 1)
+                previousObserverField = workItem.Revisions[context.WorkItemRevision - 2].Fields[_config.ObserversFieldName];
+
+            HashSet<string> uniqueObservers = null;
+
+            if (currentObserverField != null)
             {
-                string observerString = Convert.ToString(observerField.Value);
-                _log.DebugFormat("Found field containing observers. Fieldname is: {0}, Observers are: {1}", _config.ObserversFieldName, observerString);
+                uniqueObservers = new HashSet<string>(GetObserversFromText(Convert.ToString(currentObserverField.Value)));
 
-                if (observerString != null && observerString.Trim().Length > 0)
-                {
-                    _log.DebugFormat("Extracting Observers using RegexPattern: {0}, RegexOptions: {1}, Observers: {2}", _config.RegexPattern, _config.RegexOptions, observerString);
-
-                    MatchCollection matches = Regex.Matches(observerString, _config.RegexPattern, _config.RegexOptions);
-
-                    try
-                    {
-                        foreach (Match match in matches)
-                        {
-                            string user = match.Groups["user"].Value.Trim();
-
-                            if (user.Length > 0)
-                            {
-                                _log.DebugFormat("Found Observer: {0}.", user);
-                                observers.Add(user.Trim());
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        _log.ErrorFormat("Error while trying to extract observers: {0}", ex);
-                        throw;
-                    }
-                }
+                if (previousObserverField != null)
+                    uniqueObservers.UnionWith(GetObserversFromText(Convert.ToString(previousObserverField.Value)));
             }
             else
                 _log.WarnFormat("Could not find field containing observers. Fieldname is: {0}", _config.ObserversFieldName);
 
             _log.Debug("Finish: Getting List of Observers.");
+
+            List<string> result = new List<string>();
+
+            if (uniqueObservers != null)
+            {
+                foreach (string observer in uniqueObservers)
+                {
+                    result.Add(observer);
+                }
+            }
+
+            return result;
+        }
+
+        private List<string> GetObserversFromText(string observerString)
+        {
+            List<string> observers = new List<string>();
+            _log.DebugFormat("Found field containing observers. Fieldname is: {0}, Observers are: {1}", _config.ObserversFieldName, observerString);
+
+            if (observerString != null && observerString.Trim().Length > 0)
+            {
+                _log.DebugFormat("Extracting Observers using RegexPattern: {0}, RegexOptions: {1}, Observers: {2}", _config.RegexPattern, _config.RegexOptions, observerString);
+
+                MatchCollection matches = Regex.Matches(observerString, _config.RegexPattern, _config.RegexOptions);
+
+                try
+                {
+                    foreach (Match match in matches)
+                    {
+                        string user = match.Groups["user"].Value.Trim();
+
+                        if (user.Length > 0)
+                        {
+                            _log.DebugFormat("Found Observer: {0}.", user);
+                            observers.Add(user.Trim());
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _log.ErrorFormat("Error while trying to extract observers: {0}", ex);
+                    throw;
+                }
+            }
+
             return observers;
         }
 
