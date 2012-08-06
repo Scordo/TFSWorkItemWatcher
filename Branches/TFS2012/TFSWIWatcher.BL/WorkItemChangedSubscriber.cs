2d7662b3d097bc180bd9ad0670825b080ac2add2
Scordo_cp <SND\Scordo_cp>
2012-08-06 20:37:37 +0000

    Implemented the frame of the new WorkitemChangedSubscriber

--- Branches/TFS2012/TFSWIWatcher.BL/WorkItemChangedSubscriber.cs
+++ Branches/TFS2012/TFSWIWatcher.BL/WorkItemChangedSubscriber.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using Microsoft.TeamFoundation.Common;
 using Microsoft.TeamFoundation.Framework.Server;
 using Microsoft.TeamFoundation.WorkItemTracking.Server;
+using log4net;
 
 namespace TFSWIWatcher.BL
 {
     public class WorkItemChangedSubscriber : ISubscriber
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(WorkItemChangedSubscriber));
+
         string ISubscriber.Name
         {
             get { return "TFS Workitem Watcher"; }
@@ -21,9 +25,28 @@ namespace TFSWIWatcher.BL
             return new[] { typeof(WorkItemChangedEvent) };
         }
 
-        EventNotificationStatus ISubscriber.ProcessEvent(TeamFoundationRequestContext requestContext, NotificationType notificationType, object notificationEventArgs, out int statusCode, out string statusMessage, out Microsoft.TeamFoundation.Common.ExceptionPropertyCollection properties)
+        EventNotificationStatus ISubscriber.ProcessEvent(TeamFoundationRequestContext requestContext, NotificationType notificationType, object notificationEventArgs, out int statusCode, out string statusMessage, out ExceptionPropertyCollection properties)
         {
-            throw new NotImplementedException();
+            statusCode = 0;
+            properties = null;
+            statusMessage = String.Empty;
+
+            try
+            {
+                if (notificationType == NotificationType.Notification && notificationEventArgs is WorkItemChangedEvent)
+                    OnWorkitemChanged(notificationEventArgs as WorkItemChangedEvent);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex);
+            }
+
+            return EventNotificationStatus.ActionPermitted;
+        }
+
+        private void OnWorkitemChanged(WorkItemChangedEvent workItemChangedEvent)
+        {
+            // TODO: Implement stuff here
         }
     }
 }
