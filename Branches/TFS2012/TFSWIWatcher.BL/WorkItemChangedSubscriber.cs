a916ae3fb6cbe3d413508f887c95237760b30fbd
Scordo_cp <SND\Scordo_cp>
2012-08-06 20:27:23 +0000

    Added incomplete serverside workitem changed subscriber and dependencies

---
+++ Branches/TFS2012/TFSWIWatcher.BL/WorkItemChangedSubscriber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.TeamFoundation.Framework.Server;
+using Microsoft.TeamFoundation.WorkItemTracking.Server;
+
+namespace TFSWIWatcher.BL
+{
+    public class WorkItemChangedSubscriber : ISubscriber
+    {
+        string ISubscriber.Name
+        {
+            get { return "TFS Workitem Watcher"; }
+        }
+
+        SubscriberPriority ISubscriber.Priority
+        {
+            get { return SubscriberPriority.Normal; }
+        }
+
+        Type[] ISubscriber.SubscribedTypes()
+        {
+            return new[] { typeof(WorkItemChangedEvent) };
+        }
+
+        EventNotificationStatus ISubscriber.ProcessEvent(TeamFoundationRequestContext requestContext, NotificationType notificationType, object notificationEventArgs, out int statusCode, out string statusMessage, out Microsoft.TeamFoundation.Common.ExceptionPropertyCollection properties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
