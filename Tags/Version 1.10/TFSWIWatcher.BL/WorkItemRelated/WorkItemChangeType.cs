7fb194b2937de2bf11e9b664f56bffd8eb4a09d3
Scordo_cp <SND\Scordo_cp>
2012-08-07 14:41:12 +0000

    Created release branch for TFS 2010 version

---
+++ Tags/Version 1.10/TFSWIWatcher.BL/WorkItemRelated/WorkItemChangeType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace TFSWIWatcher.BL.WorkItemRelated
+{
+    [XmlType, Serializable]
+    public enum WorkItemChangeType
+    {
+        /// <summary>
+        /// The Work Item has been created
+        /// </summary>
+        New,
+
+        /// <summary>
+        /// The Work Item has been changed
+        /// </summary>
+        Change
+    }
+}
---
+++ Tags/Version 1.10/TFSWIWatcher.Resources/ReleaseStuff/CreateRelease.bat
@@ -0,0 +1,2 @@
+%WINDIR%\Microsoft.NET\Framework\v3.5\MSBuild.exe Release.target /p:Release_Destination_Base_Path=C:\Releases\TFSWIWatcher\
+pause
