e59b75c11c5578ea4150068e5b81c8f16ece475a
Scordo_cp <SND\Scordo_cp>
2013-09-04 20:08:59 +0000

    Created Tag for VS 2012 release

---
+++ Tags/Version 1.2/TFSWIWatcher.BL/WorkItemChangeInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace TFSWIWatcher.BL
+{
+    public class WorkItemChangeInfo
+    {
+        public string PortfolioProject { get; set; }
+        public string WorkItemTitle { get; set; }
+        public string WorkitemType { get; set; }
+    }
+}
---
+++ Tags/Version 1.2/TFSWIWatcher.BL/WorkItemChangedContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Server;
+using TFSWIWatcher.BL.Configuration;
+
+namespace TFSWIWatcher.BL
+{
+    public class WorkItemChangedContext
+    {
+        #region Properties
+
+        public WorkItemChangedEvent WorkItemChangedEvent
+        {
+            get; internal set;
+        }
+
+        public WorkItemChangeInfo WorkItemChangeInfo
+        {
+            get; internal set;
+        }
+
+        public TfsTeamProjectCollection TeamProjectCollection
+        {
+            get; internal set;
+        }
+
+        public int WorkItemID
+        {
+            get; internal set;
+        }
+
+        public int WorkItemRevision
+        {
+            get; internal set;
+        }
+
+        public SubscriberConfig ConfigSettings
+        {
+            get;
+            internal set;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public WorkItemChangedContext(WorkItemChangeInfo changeInfo, TfsTeamProjectCollection teamProjectCollection, int workItemID, int workItemRevision, SubscriberConfig configSettings, WorkItemChangedEvent workItemChangedEvent)
+        {
+            WorkItemChangeInfo = changeInfo;
+            TeamProjectCollection = teamProjectCollection;
+            WorkItemID = workItemID;
+            WorkItemRevision = workItemRevision;
+            ConfigSettings = configSettings;
+            WorkItemChangedEvent = workItemChangedEvent;
+        }
+
+        #endregion
+    }
+}
