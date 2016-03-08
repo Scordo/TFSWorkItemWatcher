aaa1f1906c4c1bb477cc90bbafb463540c8a3962
Scordo_cp <SND\Scordo_cp>
2016-03-08 15:52:22 +0000

    Deleted TFS 2013 dev branch

--- Branches/TFS2013/TFSWIWatcher.BL/WorkItemChangeInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TFSWIWatcher.BL
-{
-    public class WorkItemChangeInfo
-    {
-        public string PortfolioProject { get; set; }
-        public string WorkItemTitle { get; set; }
-        public string WorkitemType { get; set; }
-    }
-}
--- Branches/TFS2013/TFSWIWatcher.Resources/ReleaseStuff/CreateRelease.bat
+++
@@ -1,3 +0,0 @@
-"%programfiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" Release.target /p:Release_Destination_Base_Path=C:\Releases\TFSWIWatcher\
-pause
-
