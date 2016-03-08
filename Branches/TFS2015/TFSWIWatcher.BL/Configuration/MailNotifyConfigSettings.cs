e30ef744c610be8f294315a5742ee40fa40a8e6f
Scordo_cp <SND\Scordo_cp>
2016-03-08 15:53:25 +0000

    Dev-Branch for TFS 2015

---
+++ Branches/TFS2015/TFSWIWatcher.BL/Configuration/MailNotifyConfigSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace TFSWIWatcher.BL.Configuration
+{
+    public class MailNotifyConfigSettings
+    {
+        #region Properties
+
+        public string MailTransformationFile { get;internal set; }
+        public string DevMail { get; internal set; }
+
+        #endregion
+    }
+}
